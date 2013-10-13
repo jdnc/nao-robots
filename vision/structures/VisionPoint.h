760330befd663e0599b4d4944cf12dac7b6ee8ed
Madhura <madhuraparikh@gmail.com>
2013-10-12 20:33:16 -0500

    Added parent and next pointers

--- vision/structures/VisionPoint.h
+++ vision/structures/VisionPoint.h
@@ -7,5 +7,7 @@ struct VisionPoint {
   uint16_t xi, xf, dx, yi, yf, dy;
   uint16_t lbIndex;
   bool isValid;
+  struct VisionPoint* parent; // poiints to the parent of a region
+  struct VisionPoint* next; // points to the next run in its region
 };
 #endif
