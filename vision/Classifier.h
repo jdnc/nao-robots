9c5b67d52ea78b561c0361bf4ae8f22d27079fdf
mparikh <madhuraparikh@gmail.com>
2013-11-21 23:40:25 -0600

    more changes to classifier.h

--- vision/Classifier.h
+++ vision/Classifier.h
@@ -2,8 +2,13 @@
 #define CLASSIFIER_H
 
 #include<opencv2/core/core.hpp>
-#include<opencv2/highgui/highgui.hpp>
-#include<opencv2/video/tracking.hpp>
+#include <opencv2/video/tracking.hpp>
+#include <opencv2/video/video.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+
 
 #include <math/Point.h>
 #include <list>
